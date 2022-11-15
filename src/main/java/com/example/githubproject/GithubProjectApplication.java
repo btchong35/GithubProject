cbfcc16b58a2b40d15f4dec2ade171eda8aa7c2c
btchong35 <btchong35@126.com>
2022-11-15 15:12:33 +0800

    11.15 15:12

--- src/main/java/com/example/githubproject/GithubProjectApplication.java
+++ src/main/java/com/example/githubproject/GithubProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
-15:10
+15:12
  */
 @SpringBootApplication
 public class GithubProjectApplication {
